bd994bda904adb6b5a5ee10598d927300c91445d
Camilo12p <wilmer12yt@gmail.com>
2024-03-04 16:14:37 -0500

    estado de prueba del rompimiento del programa

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ import external_data.readCsv as r1
 
 
 if __name__ == '__main__' :
+    core.clearScreen()
     print('Bienvenidos al programa de Administracion de activos de campus')
     print('Reglas\n1.Para cancelar cualquier operacion ctrl + c\n2.Los nombres de las zonas y personas no pueden contener numeros')
     print('3.Los email deben tener un @\n4. los codigos de campus del activo siempre son en mayuscula, asi usted los escriba en minuscual')
     print('5. los id, telefonos de las personas solo pueden ser numeros\n6.nro de formulario,codigo de transaccion,valor unitario de los activos solo son numeros')
     core.pauseScreen()
-    
+
     Inventario_Campus={
         'Personas':{},
         'Zonas':{},
